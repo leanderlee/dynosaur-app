ad5cdee7f5a810c9347c15927bd7401c49378955
Leander (Laptop) <me@leander.ca>
2013-11-19 22:50:42 -0800

    Added options to apps

--- pages/app.js
+++ pages/app.js
@@ -1,4 +1,16 @@
 App.populator('app', function (page, app) {
 	$(page).find(".app-title").text(app.name);
-	$(".app-description", page).text(app.description);
+	$(page).find(".app-description").text(app.description);
+	if (!app.options.length) {
+		$("#options-list", page).text("There are no options.");
+	} else {
+		$("#options-list", page).empty();
+	}
+	for (var i = 0; i < app.options.length; i++) {
+		var source = $("#option-" + app.options[i].type + "-template").html();
+		var template = swig.compile(source);
+		var html = template({ option: app.options[i] });
+		$(html).appendTo($("#options-list", page));
+	}
+	console.log(app.options);
 });
--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,19 @@
 
 App.populator('home', function (page) {
 	var apps = [
-		{ name: "Awesome App 1", description: "This is a description of app 1.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
-		{ name: "Awesome App 2", description: "This is a description of app 2.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
-		{ name: "Awesome App 3", description: "This is a description of app 3.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
-		{ name: "Awesome App 4", description: "This is a description of app 4.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
-		{ name: "Awesome App 5", description: "This is a description of app 5.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
-		{ name: "Awesome App 6", description: "This is a description of app 6.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
-		{ name: "Awesome App 7", description: "This is a description of app 7.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
-		{ name: "Awesome App 8", description: "This is a description of app 8.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 1", description: "This is a description of app 1.",
+			options: [
+				{ property: "name", label: "Name", type: "text", "default": "unnamed app" },
+				{ property: "gender", label: "Sex", type: "select", options: [{ value: "m", label: "Male" }, { value: "f", label: "Female" }] },
+			]
+		},
+		{ name: "Awesome App 2", description: "This is a description of app 2.", options: [{ property: "name", label: "Name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 3", description: "This is a description of app 3.", options: [{ property: "name", label: "Name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 4", description: "This is a description of app 4.", options: [{ property: "name", label: "Name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 5", description: "This is a description of app 5.", options: [{ property: "name", label: "Name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 6", description: "This is a description of app 6.", options: [{ property: "name", label: "Name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 7", description: "This is a description of app 7.", options: [{ property: "name", label: "Name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 8", description: "This is a description of app 8.", options: [{ property: "name", label: "Name", type: "text", "default": "unnamed app" }] },
 	];
 	var source = $("#app-template").html();
 	var template = swig.compile(source);

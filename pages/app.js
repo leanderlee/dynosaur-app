d7679d02d0288806e7c446b726ee1f073f2c5bc1
Leander (Laptop) <me@leander.ca>
2013-11-19 21:23:39 -0800

    Initial app page design

--- pages/app.js
+++ pages/app.js
@@ -1,3 +1,4 @@
 App.populator('app', function (page, app) {
 	$(".app-title", page).text(app.name);
+	$(".app-description", page).text(app.description);
 });
--- pages/home.js
+++ pages/home.js
@@ -1,18 +1,19 @@
 
 App.populator('home', function (page) {
 	var apps = [
-		{ name: "Awesome App 1", description: "This is a description of app 1."},
-		{ name: "Awesome App 2", description: "This is a description of app 2."},
-		{ name: "Awesome App 3", description: "This is a description of app 3."},
-		{ name: "Awesome App 4", description: "This is a description of app 4."},
-		{ name: "Awesome App 5", description: "This is a description of app 5."},
-		{ name: "Awesome App 6", description: "This is a description of app 6."},
-		{ name: "Awesome App 7", description: "This is a description of app 7."},
-		{ name: "Awesome App 8", description: "This is a description of app 8."},
+		{ name: "Awesome App 1", description: "This is a description of app 1.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 2", description: "This is a description of app 2.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 3", description: "This is a description of app 3.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 4", description: "This is a description of app 4.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 5", description: "This is a description of app 5.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 6", description: "This is a description of app 6.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 7", description: "This is a description of app 7.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
+		{ name: "Awesome App 8", description: "This is a description of app 8.", options: [{ property: "name", type: "text", "default": "unnamed app" }] },
 	];
 	var source = $("#app-template").html();
 	var template = swig.compile(source);
 
+	App.load("app", apps[0]);
 	for (var i = 0; i < apps.length; i++) {
 		var html = template({ app: apps[i] });
 		$(html).data("app", i).on('click', function () {

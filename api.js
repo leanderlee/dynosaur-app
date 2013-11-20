d691d9d3539a6a7a7c9ef0327f191600d365ac05
Leander (Laptop) <me@leander.ca>
2013-11-19 18:56:51 -0800

    Added endpoints for API.js

--- api.js
+++ api.js
@@ -2,31 +2,40 @@ var DynoAPI = function () {
 	var self = {};
 
 	// Private methods
-	var request = function (url, data, callback) {
-		callback = callback || function(){};
-		var data = $.extend({}, data);
-		$.ajax({
-			url: url,
-			type: "POST",
-			data: data,
-			dataType: "json",
-			success: function (resp) {
-				callback.call(null, resp);
-			}
-		});
+	var request = function (method) {
+		return function (url, data, callback) {
+			callback = callback || function(){};
+			var data = $.extend({}, data);
+			$.ajax({
+				url: url,
+				type: method,
+				data: data,
+				dataType: "json",
+				success: function (resp) {
+					callback.call(null, resp);
+				}
+			});
+		};
 	};
-
+	var get = request("GET");
+	var post = request("POST");
+	
 	// Public methods
 	self.login = function (email, password, callback) {
 		callback = callback || function(){};
-		request("/auth/login", { email: email, password: password }, function (data) {
-			if (data.success) {
-				callback.call(null, null, data.user);
-			} else {
-				callback.call(null, data);
-			}
-		});
+		get("/login", { email: email, password: password }, callback);
 	};
-	
+	self.isLoggedIn = function (callback) {
+		callback = callback || function(){};
+		get("/logged_in", { email: email, password: password }, callback);
+	};
+	self.create = function (name, type, options, callback) {
+		callback = callback || function(){};
+		name = name || "";
+		type = type || "";
+		options = JSON.stringify(options || {});
+		post("/create", { name: name, type: type, options: options }, callback);
+	};
+
 	return self;
 };
